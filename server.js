99471ec4e8ca49ac87f814e55f06e6668aabfe6f
dungnt-hust <tiendung160301@gmail.com>
2023-03-10 22:07:41 +0700

    create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+//create express app
+const app = express();
+
+//Setup server port
+const port = process.env.PORT || 5000;
+
+//parse requests
+app.use(bodyParser.urlencoded({extended: true}));
+
+//parse requests .... json
+app.use(bodyParser.json());
+
+//define a root route
+app.get('/', (req, res) => {
+    res.send('Welcome to my website');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
